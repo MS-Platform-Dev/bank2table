ecf1a80e2e4e991eda5bc71298c23a62f3690403
MS-Platform-Dev <85239926+MS-Platform-Dev@users.noreply.github.com>
2022-04-25 12:55:24 +0200

    Add play code in guiforms.py

--- guiforms.py
+++ guiforms.py
@@ -2,5 +2,14 @@
 
 def gui_banner():
 # Simple welcome page
-    pass
+
+    # Play/test code
+    layout = [[sg.Text('Just a number')],
+                     [sg.InputText()],
+                     [sg.Submit(), sg.Cancel()]]
+    window = sg.Window('Simple Number', layout)
+    event, values = window.read()
+    window.close()
+    text_input = values[0]
+    sg.popup('You entered', text_input)
 
