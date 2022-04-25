dae85044fbb674451cb96c0f215454bc61c73110
unknown <>
2022-04-25 13:05:34 +0200

    Add more code to guiforms.py

--- guiforms.py
+++ guiforms.py
@@ -13,3 +13,21 @@ def gui_banner():
     text_input = values[0]
     sg.popup('You entered', text_input)
 
+
+def twonums():
+    layout = [[sg.Text('Just a number')], 
+              [sg.InputText()],
+              [sg.InputText()],
+              [sg.Submit(), sg.Cancel()]]
+
+    window = sg.Window('Simple Number', layout)
+
+    event, values = window.read()
+    window.close()
+
+    text_input1 = values[0]
+    text_input2 = values[1]
+    print(event)
+    print(values)
+    sg.popup('You entered', text_input1 + " " + text_input2)
+
