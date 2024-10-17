f9c09c1778fb7ddfc69973783bc00fb632ab9b81
MS-Platform-Dev <85239926+MS-Platform-Dev@users.noreply.github.com>
2024-10-17 08:50:47 +0000

    Update connectors.py
    Cosmetic changes

--- connectors.py
+++ connectors.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Copyright © 2024 MonaCompta
+# Copyright ©2024 MonaCompta
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
 # (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
@@ -19,12 +19,13 @@
 # Description:  Library of connectors to different banks
 #
 # Author:       Mike
-# Copyright:    (c) 2024 MonaCompta
+# Copyright:    ©2024 MonaCompta
 # Licence:      MIT
 
 # Change Log:
 # DATE          VERSION       DESCRIPTION
 # 2024-02-02    0.1  alpha    Initial file
+# 2024-10-17    0.2  alpha    Cosmetic changes
 #-------------------------------------------------------------------------------
 
 from datetime import datetime
