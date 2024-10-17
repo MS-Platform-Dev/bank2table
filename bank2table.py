ced372dda415df5d75786701d35fcb0bb0c4caec
MS-Platform-Dev <85239926+MS-Platform-Dev@users.noreply.github.com>
2024-10-17 08:48:52 +0000

    Update bank2table.py
    Bumped version

--- bank2table.py
+++ bank2table.py
@@ -28,7 +28,8 @@
 # 2022-01-24    0.2  alpha    Re-created for new repo. Changed license to MIT.
 #                             Added skeleton functions
 # 2022-04-19    0.3  alpha    Added GUI placeholder
-# 2023-11-20    0.4  alpha    Versison refresh
+# 2023-11-20    0.4  alpha    Version refresh
+# 2024-10-17    0.5  alpha    Bumped version
 #-------------------------------------------------------------------------------
 
 from datetime import datetime
