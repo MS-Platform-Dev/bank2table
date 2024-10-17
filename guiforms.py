62ecf5650600b1acd6ab3e35700dda716fbbbacb
MS-Platform-Dev <85239926+MS-Platform-Dev@users.noreply.github.com>
2024-10-17 08:56:48 +0000

    Add copyright banner

--- guiforms.py
+++ guiforms.py
@@ -1,4 +1,34 @@
-# Module to hold the UI forms
+#-------------------------------------------------------------------------------
+# Copyright ©2024 MonaCompta
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#-------------------------------------------------------------------------------
+
+# Module:       guiforms.py
+# Description:  Module to hold the UI forms
+
+# Author:       Mike
+# Copyright:    ©2024 MonaCompta
+# Licence:      MIT
+
+# Change Log:
+# DATE          VERSION       DESCRIPTION
+# 2022-04-18    0.1  alpha    Initial commit
+# 2024-10-17    0.2  alpha    Add copyright banner
+#-------------------------------------------------------------------------------
+
+
 import PySimpleGUI as sg
 
 def gui_banner():
