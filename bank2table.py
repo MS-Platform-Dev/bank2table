39d3302c31161b1cc0b99e6d9526fb316f5ec3e4
MS-Platform-Dev <85239926+MS-Platform-Dev@users.noreply.github.com>
2024-10-20 09:33:33 +0000

    Update bank2table.py
    Correct copyright

--- bank2table.py
+++ bank2table.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Copyright © 2024 MonaCompta
+# Copyright ©2024 MonaCompta
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
 # (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
@@ -19,7 +19,7 @@
 # Description:  Convert PDF bank statements to data tables for processing
 #
 # Author:       Mike
-# Copyright:    (c) 2022 MonaCompta
+# Copyright:    ©2024 MonaCompta
 # Licence:      MIT
 
 # Change Log:
